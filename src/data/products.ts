83c221619b01f4802510b15516003793a74322c6
jeangoi483-droid <jeangoi483@gmail.com>
2025-12-14 07:47:08 +0000

    Correction des ID pour Vercel

--- src/data/products.ts
+++ src/data/products.ts
@@ -2,28 +2,28 @@
 
 export const mockProducts = [
   {
-    id: 1,
+    id: "1",
     name: "Montre S-Series Luxe",
     price: 85000,
-    image: "/images/montre-s.jpg", // Assurez-vous de l'extension (.jpg ou .png)
+    image: "/images/montre-s.jpg",
     description: "Une montre élégante au design minimaliste, parfaite pour toutes les occasions."
   },
   {
-    id: 2,
+    id: "2",
     name: "Smartwatch Pro Connect",
     price: 125000,
     image: "/images/montre-connecte.jpg",
     description: "Suivez votre santé et restez connecté avec style. Écran HD et autonomie longue durée."
   },
   {
-    id: 3,
+    id: "3",
     name: "Montre Curren Business",
     price: 45000,
     image: "/images/montre-curren-1.jpg",
     description: "Le chronographe classique pour l'homme moderne. Précision et robustesse au rendez-vous."
   },
   {
-    id: 4,
+    id: "4",
     name: "Montre Quartz Classic",
     price: 35000,
     image: "/images/montre-quartz.jpg",
