17fbcc5839c870dbe4366aaeed814d5d2135fa41
jeangoi483-droid <jeangoi483@gmail.com>
2026-02-03 15:15:37 +0100

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -96,11 +96,11 @@ export const mockProducts = [
   name: "Gk Montre Dame Cadran Rond - Argent",
   price: 4000,
   description: "Cette montre raffinée de la collection GK combine élégance et fonctionnalité. Son boîtier délicat en acier inoxydable argenté, orné d’un cadran nacré aux détails subtils, en fait un accessoire intemporel. Le bracelet en cuir véritable, doux au toucher, ajoute une touche de confort et de sophistication à ce modèle. Ses aiguilles dorées et ses index lumineux offrent une lisibilité parfaite, même en faible lumière. Un choix idéal pour compléter n’importe quelle tenue, cette montre incarne la féminité et l’élégance discrète..",
-  image: "/images/gk.jpg",
+  image: "/images/montre12.jpg",
   category: "Luxe"
 },
   {
-  id: "12",
+  id: "14",
   name: "Montre De Sport A Quartz - Marron / Noir ",
   price: 3000,
   description: "Type de produit : Montre dame
