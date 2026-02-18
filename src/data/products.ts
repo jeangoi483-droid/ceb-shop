3190610463c9f85a881123ad1f7b2ec4094f9d1f
jeangoi483-droid <jeangoi483@gmail.com>
2026-02-18 14:39:41 +0000

    Fix formatting of product name and description

--- src/data/products.ts
+++ src/data/products.ts
@@ -85,9 +85,9 @@ export const mockProducts = [
 },
   {
   id: "12",
-  name: "Montre De Sport A Quartz - Marron / Noir ",
+  name: "Montre De Sport A Quartz - Marron / Noir",
   price: 15000,
-  description: "Le design unique de cette montre convient à la fois pour une apparence formelle et décontractée." 
+  description: "Le design unique de cette montre convient à la fois pour une apparence formelle et décontractée.", 
   image: "/images/montre12.jpg",
   category: "Luxe"
 },
