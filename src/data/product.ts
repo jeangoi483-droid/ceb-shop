fb20065c6b49a2602465a43751c3bbe47d8eea69
jeangoi483-droid <jeangoi483@gmail.com>
2026-02-26 16:27:34 +0000

    Create mockProducts array with product details
    Added mock product data for e-commerce application.

---
+++ src/data/product.ts
@@ -0,0 +1,126 @@
+// src/data/products.ts
+
+export const mockProducts = [
+  {
+    id: "1",
+    name: "Nibosi Montre",
+    price: 13000,
+    image: "/images/NIBOSI.jpg",
+    description: "Une montre élégante au design minimaliste, parfaite pour toutes les occasions."
+  },
+  {
+    id: "2",
+    name: "Smartwatch Pro Connect",
+    price: 12000,
+    image: "/images/montre-connecte.jpg",
+    description: "Suivez votre santé et restez connecté avec style. Écran HD et autonomie longue durée."
+  },
+  {
+    id: "3",
+    name: "Montre Curren Business",
+    price: 25000,
+    image: "/images/montre-curren-1.jpg",
+    description: "Le chronographe classique pour l'homme moderne. Précision et robustesse au rendez-vous."
+  },
+  {
+    id: "4",
+    name: "Montre Quartz Classic",
+    price: 12000,
+    image: "/images/montre-quartz.jpg",
+    description: "La fiabilité du mouvement Quartz dans un boîtier raffiné et un bracelet confortable."
+  },
+  {
+  id: "5",
+  name: "Skmei Montre Digitale Homme Multifonction - Bracelet Acier",
+  price: 10000,
+  description: "Montre digitale moderne avec écran clair,bracelet en acier inoxydable et afffffichage complet (heure,date, jour, chronomètre). design élégant et robuste, idéale pour un usage quotidien, travail ou sorties.",
+  image: "/images/montre-skmei.jpg",
+  category: "Luxe"
+},
+  {
+    id: "6",
+    name: "Curren Chronographe Acier – Élégance Sport",
+    price: 25000,
+    image: "/images/Curren-Chronographe-Acier.jpeg",
+    description: "Montre Curren pour homme au design sport chic, boîtier robuste en acier inoxydable, chronographes fonctionnels et affichage date. Idéale pour un style moderne et affirmé, au quotidien comme en soirée."
+  },
+  {
+    id: "7",
+    name: "Curren Chronographe Cuir",
+    price: 25000,
+    image: "/images/Curren-Chronographe-Cuir.jpeg",
+    description: "Montre Curren élégante avec bracelet en cuir premium, boîtier finition dorée et cadran chronographe raffiné. Parfaite pour les hommes qui recherchent distinction, confort et style intemporel."
+  },
+  {
+  id: "8",
+  name: "Swatch Swiss Silicone – Minimalisme Moderne",
+  price: 20000,
+  description: "Montre Swatch Swiss au design épuré et contemporain, bracelet en silicone souple et léger, affichage clair avec date. Un choix idéal pour un look décontracté, urbain et tendance.",
+  image: "/images/Swatch-Swiss-Silicone.jpeg",
+  category: "Luxe"
+},
+  {
+  id: "9",
+  name: "Montre SK Black Edition",
+  price: 12000,
+  description: "Cette montre élégante et moderne de la marque SK se distingue par son design raffiné. Son boîtier noir minimaliste et son bracelet en cuir noir apportent une touche sophistiquée à votre look. Les index en cuivre et les aiguilles fines ajoutent une subtile touche de contraste, offrant ainsi une lisibilité parfaite tout en restant élégante. Parfaite pour toutes les occasions, cette montre allie style et simplicité.",
+  image: "/images/montr10.JPG",
+  category: "Luxe"
+},
+  {
+  id: "10",
+  name: "Montre d'exercice lumineuse en silicone pour femmes carrée - Blanc crème",
+  price: 10000,
+  description: "Montre Swatch Swiss au design épuré et contemporain, bracelet en silicone souple et léger, affichage clair avec date. Un choix idéal pour un look décontracté, urbain et tendance.",
+  image: "/images/montre10.jpg",
+  category: "Luxe"
+},
+  {
+  id: "11",
+  name: "Casio Montre Femme classique ",
+  price: 30000,
+  description: "LTP-V005L-1BUDF - Bracelet en cuir - Resistant à l'eau.",
+  image: "/images/casio.jpg",
+  category: "Luxe"
+},
+  {
+  id: "12",
+  name: "Montre De Sport A Quartz - Marron / Noir",
+  price: 15000,
+  description: "Le design unique de cette montre convient à la fois pour une apparence formelle et décontractée.", 
+  image: "/images/montre12.jpg",
+  category: "Luxe"
+},
+  {
+  id: "13",
+  name: "Gk Montre Dame Cadran Rond - Argent",
+  price: 5000,
+  description: "Cette montre raffinée de la collection GK combine élégance et fonctionnalité. Son boîtier délicat en acier inoxydable argenté, orné d’un cadran nacré aux détails subtils, en fait un accessoire intemporel. Le bracelet en cuir véritable, doux au toucher, ajoute une touche de confort et de sophistication à ce modèle. Ses aiguilles dorées et ses index lumineux offrent une lisibilité parfaite, même en faible lumière. Un choix idéal pour compléter n’importe quelle tenue, cette montre incarne la féminité et l’élégance discrète..",
+  image: "/images/gk.jpg",
+  category: "Luxe"
+},
+  {
+  id: "14",
+  name: "Amber Time",
+  price: 7000,
+  description: "Montre de luxe résistant à l'eau ",
+  image: "/images/ambertime.JPG",
+  category: "Luxe"
+},
+  {
+  id: "15",
+  name: "Montre BINBOND Bleue Homme",
+  price: 10000,
+  description: "Offrez-vous l'élégance audacieuse avec cette montre BINBOND au design unique. Son boîtier et bracelet en acier inoxydable bleu métallisé ajoutent une touche moderne et dynamique, tandis que son cadran avec chiffres romains sculptés donne à cette montre un aspect à la fois sportif et sophistiqué. Son mécanisme robuste et son style distinctif en font l'accessoire idéal pour l'homme moderne qui cherche à allier performance et esthétique. Parfaite pour les occasions décontractées ou pour sublimer une tenue plus habillée, cette montre attire l’attention avec subtilité et caractère.",
+  image: "/images/binbond.JPG",
+  category: "Luxe"
+},
+  {
+  id: "16",
+  name: "Curren Montre De Luxe Pour Hommes",
+  price: 35000,
+  description: "Cadran de conception de haute qualité de luxe de marque supérieure, fonction d'affichage de la date",
+  image: "/images/curren16.jpg",
+  category: "Luxe"
+},
+];
