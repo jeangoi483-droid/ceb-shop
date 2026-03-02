a193b16c43d910bd275ef7d1f34b78248d81b608
jeangoi483-droid <jeangoi483@gmail.com>
2026-03-02 11:43:00 +0000

    Revise product entries in mockProduct array
    Updated product details and removed several items from the mock product list.

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,126 +1,33 @@
-// src/data/products.ts
+// src/data/product.ts
 
 export const mockProduct = [
   {
-    id: "1",
-    name: "AIDAILU Sandales transparentes à talon avec bijou strass",
+    id: "101",
+    name: "AIDAILU Sandales transparentes à talon",
     price: 6000,
     image: "/images/chaussure1.jpg",
-    description: "Apportez une touche glamour et raffinée à vos tenues avec ces magnifiques sandales transparentes à talon, Ces sandales allient féminité, élégance et modernité pour un look chic et sophistiqué."
+    description: "Apportez une touche glamour et raffinée à vos tenues avec ces magnifiques sandales à talon."
   },
   {
-    id: "2",
-    name: "AIDAILU Sandales fines à talon transparent avec bride cheville",
+    id: "102",
+    name: "AIDAILU Sandales fines à talon transparent",
     price: 6000,
     image: "/images/chaussure2.jpg",
-    description: "Sublimez votre silhouette avec ces sandales délicates à talon transparent. Leur design minimaliste et brillant épouse parfaitement le pied pour un rendu à la fois sexy et élégant."
+    description: "Sublimez votre silhouette avec ces sandales délicates à talon transparent."
   },
   {
-    id: "3",
+    id: "103",
     name: "Mateamoda",
     price: 6000,
     image: "/images/chaussure3.jpg",
-    description: "L'empeigne est en cuir pu de haute qualité avec une fabrication exquise. Le talon épais de cette chaussure vous permet de marcher confortablement et de manière stable."
+    description: "L'empeigne en cuir PU de haute qualité, talon épais confortable et stable."
   },
   {
-    id: "4",
-    name: "JB Mule Sandales argentées à brides multiples et talon aiguille ",
+    id: "104",
+    name: "JB Mule Sandales argentées à brides multiples",
     price: 22000,
     image: "/images/chaussure4.jpg",
-    description: "Affirmez votre style avec ces sandales argentées au design audacieux. Les multiples brides croisées apportent maintien et sophistication, tandis que le talon aiguille affine la silhouette."
+    description: "Sandales argentées au design audacieux, maintien et sophistication avec talon aiguille."
   },
-  {
-  id: "5",
-  name: "Skmei Montre Digitale Homme Multifonction - Bracelet Acier",
-  price: 10000,
-  description: "Montre digitale moderne avec écran clair,bracelet en acier inoxydable et afffffichage complet (heure,date, jour, chronomètre). design élégant et robuste, idéale pour un usage quotidien, travail ou sorties.",
-  image: "/images/montre-skmei.jpg",
-  category: "Luxe"
-},
-  {
-    id: "6",
-    name: "Curren Chronographe Acier – Élégance Sport",
-    price: 25000,
-    image: "/images/Curren-Chronographe-Acier.jpeg",
-    description: "Montre Curren pour homme au design sport chic, boîtier robuste en acier inoxydable, chronographes fonctionnels et affichage date. Idéale pour un style moderne et affirmé, au quotidien comme en soirée."
-  },
-  {
-    id: "7",
-    name: "Curren Chronographe Cuir",
-    price: 25000,
-    image: "/images/Curren-Chronographe-Cuir.jpeg",
-    description: "Montre Curren élégante avec bracelet en cuir premium, boîtier finition dorée et cadran chronographe raffiné. Parfaite pour les hommes qui recherchent distinction, confort et style intemporel."
-  },
-  {
-  id: "8",
-  name: "Swatch Swiss Silicone – Minimalisme Moderne",
-  price: 20000,
-  description: "Montre Swatch Swiss au design épuré et contemporain, bracelet en silicone souple et léger, affichage clair avec date. Un choix idéal pour un look décontracté, urbain et tendance.",
-  image: "/images/Swatch-Swiss-Silicone.jpeg",
-  category: "Luxe"
-},
-  {
-  id: "9",
-  name: "Montre SK Black Edition",
-  price: 12000,
-  description: "Cette montre élégante et moderne de la marque SK se distingue par son design raffiné. Son boîtier noir minimaliste et son bracelet en cuir noir apportent une touche sophistiquée à votre look. Les index en cuivre et les aiguilles fines ajoutent une subtile touche de contraste, offrant ainsi une lisibilité parfaite tout en restant élégante. Parfaite pour toutes les occasions, cette montre allie style et simplicité.",
-  image: "/images/montr10.JPG",
-  category: "Luxe"
-},
-  {
-  id: "10",
-  name: "Montre d'exercice lumineuse en silicone pour femmes carrée - Blanc crème",
-  price: 10000,
-  description: "Montre Swatch Swiss au design épuré et contemporain, bracelet en silicone souple et léger, affichage clair avec date. Un choix idéal pour un look décontracté, urbain et tendance.",
-  image: "/images/montre10.jpg",
-  category: "Luxe"
-},
-  {
-  id: "11",
-  name: "Casio Montre Femme classique ",
-  price: 30000,
-  description: "LTP-V005L-1BUDF - Bracelet en cuir - Resistant à l'eau.",
-  image: "/images/casio.jpg",
-  category: "Luxe"
-},
-  {
-  id: "12",
-  name: "Montre De Sport A Quartz - Marron / Noir",
-  price: 15000,
-  description: "Le design unique de cette montre convient à la fois pour une apparence formelle et décontractée.", 
-  image: "/images/montre12.jpg",
-  category: "Luxe"
-},
-  {
-  id: "13",
-  name: "Gk Montre Dame Cadran Rond - Argent",
-  price: 5000,
-  description: "Cette montre raffinée de la collection GK combine élégance et fonctionnalité. Son boîtier délicat en acier inoxydable argenté, orné d’un cadran nacré aux détails subtils, en fait un accessoire intemporel. Le bracelet en cuir véritable, doux au toucher, ajoute une touche de confort et de sophistication à ce modèle. Ses aiguilles dorées et ses index lumineux offrent une lisibilité parfaite, même en faible lumière. Un choix idéal pour compléter n’importe quelle tenue, cette montre incarne la féminité et l’élégance discrète..",
-  image: "/images/gk.jpg",
-  category: "Luxe"
-},
-  {
-  id: "14",
-  name: "Amber Time",
-  price: 7000,
-  description: "Montre de luxe résistant à l'eau ",
-  image: "/images/ambertime.JPG",
-  category: "Luxe"
-},
-  {
-  id: "15",
-  name: "Montre BINBOND Bleue Homme",
-  price: 10000,
-  description: "Offrez-vous l'élégance audacieuse avec cette montre BINBOND au design unique. Son boîtier et bracelet en acier inoxydable bleu métallisé ajoutent une touche moderne et dynamique, tandis que son cadran avec chiffres romains sculptés donne à cette montre un aspect à la fois sportif et sophistiqué. Son mécanisme robuste et son style distinctif en font l'accessoire idéal pour l'homme moderne qui cherche à allier performance et esthétique. Parfaite pour les occasions décontractées ou pour sublimer une tenue plus habillée, cette montre attire l’attention avec subtilité et caractère.",
-  image: "/images/binbond.JPG",
-  category: "Luxe"
-},
-  {
-  id: "16",
-  name: "Curren Montre De Luxe Pour Hommes",
-  price: 35000,
-  description: "Cadran de conception de haute qualité de luxe de marque supérieure, fonction d'affichage de la date",
-  image: "/images/curren16.jpg",
-  category: "Luxe"
-},
+  // ... ajoute d'autres articles femme ici
 ];
