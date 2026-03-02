3dd8595a53edba5e1417b08b869809d27018ae60
jeangoi483-droid <jeangoi483@gmail.com>
2026-03-02 13:31:32 +0000

    Update page.tsx

--- src/app/femmes/page.tsx
+++ src/app/femmes/page.tsx
@@ -27,7 +27,7 @@ export default function ShopPage() {
 
       {/* GRILLE PRODUITS */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {mockProducts.map((item) => (
+        {mockProduct.map((item) => (
           <div key={item.id} className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden">
             <Link href={`/product/${item.id}`}>
               <div className="relative w-full h-64">
